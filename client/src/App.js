f5ced13714c8d91aaac2daf02e3fac4da78522d3
chr0ckbean <2leebins@naver.com>
2023-10-11 18:43:49 +0900

    feat: todo 1개 추가하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Todo from './components/Todo';
+import AddTodo from './components/AddTodo';
 
 function App() {
   const [todoItems, setTodoItems] = useState([
@@ -21,8 +22,21 @@ function App() {
     },
   ]);
 
+  // todoItems 상태에 새로운 Todo 추가
+  const addItem = (newItem) => {
+    console.log(newItem);
+
+    //newItem id 키 값, newItem done 키 값 추가
+    newItem.id = todoItems.length + 1;
+    newItem.done = false;
+
+    // todoItems 배열에 newItem 추가
+    setTodoItems([...todoItems, newItem]);
+  };
+
   return (
     <div className="App">
+      <AddTodo addItem={addItem} />
       {/* todoItems 반복, props(todo 객체)로 자식 컴포넌트에 데이터 전달 */}
       {todoItems.map((todoItems) => (
         <Todo key={todoItems.id} item={todoItems} />
