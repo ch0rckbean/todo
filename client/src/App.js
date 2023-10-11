b798cd2930c630f3237a89e7d46a70b0d73ebd20
chr0ckbean <2leebins@naver.com>
2023-10-11 18:19:30 +0900

    featL todo 리스트 보이기

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Todo from './components/Todo';
 
 function App() {
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: 1,
+      title: 'my todo1',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'my todo2',
+      done: false,
+    },
+    {
+      id: 3,
+      title: 'my todo3',
+      done: true,
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* todoItems 반복, props(todo 객체)로 자식 컴포넌트에 데이터 전달 */}
+      {todoItems.map((todoItems) => (
+        <Todo key={todoItems.id} item={todoItems} />
+      ))}
     </div>
   );
 }
